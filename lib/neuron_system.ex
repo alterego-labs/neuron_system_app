9d2078f7b7c3059b89cc55af4b711a948ac08629
Sergey Gernyak <sergeg1990@gmail.com>
2016-07-18 18:13:49 +0300

    Building new neuron basic approach

--- lib/neuron_system.ex
+++ lib/neuron_system.ex
@@ -1,6 +1,8 @@
 defmodule NeuronSystem do
   use Application
 
+  import Supervisor.Spec
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
@@ -10,11 +12,21 @@ defmodule NeuronSystem do
     children = [
       # Starts a worker by calling: NeuronSystem.Worker.start_link(arg1, arg2, arg3)
       # worker(NeuronSystem.Worker, [arg1, arg2, arg3]),
+      supervisor(NeuronSystem.Processes.Supervisor, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: NeuronSystem.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @spec build_neuron((...-> any)) :: pid
+  def build_neuron(activation_function) do
+    neuron_model = %NeuronSystem.Models.Neuron{activation_function: activation_function}
+    worker_id = (DateTime.utc_now |> DateTime.to_unix |> Integer.to_string) <> ":neuron_process"
+    worker_spec = worker(NeuronSystem.Processes.Neuron, [neuron_model], [id: worker_id])
+    {:ok, pid} = Supervisor.start_child(NeuronSystem.Processes.Supervisor, worker_spec)
+    pid
   end
 end
