e51dfe5957eed03bd56cb20172c4697fee6a4ac2
Sergey Gernyak <sergeg1990@gmail.com>
2016-08-05 16:41:42 +0300

    Added docs for the NeuronSystem.Connection module

--- lib/neuron_system/connection.ex
+++ lib/neuron_system/connection.ex
@@ -1,6 +1,17 @@
 defmodule NeuronSystem.Connection do
+  @moduledoc """
+  Contains the common function for the all types of connections.
+  """
+
   alias NeuronSystem.Models.{Connection, InConnection, OutConnection}
 
+  @doc """
+  Retrieves a source of the connection.
+
+  For InConnection it is a key which represents an input value.
+  For the other types the source is a source neuron's ID.
+  """
+  @spec source(InConnection.t | Connection.t | OutConnection.t) :: atom | bitstring
   def source(%InConnection{key: key}) do
     key
   end
