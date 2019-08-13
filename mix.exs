43d3c4573531a5007c20a14ad8c3263b52ed251c
Sergio Gernyak <sergeg1990@gmail.com>
2019-08-13 22:29:16 +0300

    Change target elixir version to 1.7

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NeuronSystem.Mixfile do
   def project do
     [app: :neuron_system,
      version: "0.1.2",
-     elixir: "~> 1.3",
+     elixir: "~> 1.7",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
